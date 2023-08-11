4145dd8d38d013a119aaebcc3bd73f1b50311ae1
osparking <jbpark03@gmail.com>
2023-08-11 21:36:32 +0900

    custom userdetails class employed

--- src/main/java/space/bum/services/JpaUserDetailsService.java
+++ src/main/java/space/bum/services/JpaUserDetailsService.java
@@ -6,17 +6,18 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import lombok.AllArgsConstructor;
 import space.bum.repositories.UserRepository;
+import space.bum.security.SecurityUser;
 
 @AllArgsConstructor
 public class JpaUserDetailsService implements UserDetailsService {
 
 	private final UserRepository userRepository;
-	
+
 	@Override
-	public UserDetails loadUserByUsername(String username) 
-			throws UsernameNotFoundException {
+	public UserDetails loadUserByUsername(String username) {
 		var user = userRepository.findUserByUsername(username);
-		return null;
+		return user.map(SecurityUser::new).orElseThrow(
+				() -> new UsernameNotFoundException("존재하지않는사용자:" + username));
 	}
 
 }
