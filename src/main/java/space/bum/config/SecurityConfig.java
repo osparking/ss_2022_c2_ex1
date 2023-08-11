c2ff0e5f0b06d73b7457d9f6eac17c1d3077cfe5
osparking <jbpark03@gmail.com>
2023-08-11 12:51:29 +0900

    just class structure for security

---
+++ src/main/java/space/bum/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package space.bum.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+	UserDetailsService userDetailsService() {
+		return null;
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+}
