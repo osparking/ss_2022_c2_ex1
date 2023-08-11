79ca48aa74680f46a7f0fefd7ee9d76cfc3f1d02
osparking <jbpark03@gmail.com>
2023-08-11 15:39:06 +0900

    user repository interface used in service

--- src/main/java/space/bum/services/JpaUserDetailsService.java
+++ src/main/java/space/bum/services/JpaUserDetailsService.java
@@ -4,12 +4,18 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import lombok.AllArgsConstructor;
+import space.bum.repositories.UserRepository;
+
+@AllArgsConstructor
 public class JpaUserDetailsService implements UserDetailsService {
 
+	private final UserRepository userRepository;
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) 
 			throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
+		var user = userRepository.findUserByUsername(username);
 		return null;
 	}
 
