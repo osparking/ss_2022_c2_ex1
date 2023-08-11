3419ca5f3da9d078727d4ced4c9ecb239d3b7ea5
osparking <jbpark03@gmail.com>
2023-08-11 21:47:10 +0900

    user service class makes a bean

--- src/main/java/space/bum/services/JpaUserDetailsService.java
+++ src/main/java/space/bum/services/JpaUserDetailsService.java
@@ -3,12 +3,14 @@ package space.bum.services;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
 import space.bum.repositories.UserRepository;
 import space.bum.security.SecurityUser;
 
 @AllArgsConstructor
+@Service
 public class JpaUserDetailsService implements UserDetailsService {
 
 	private final UserRepository userRepository;
