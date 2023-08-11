79e9aba0d9265799fd4444e88f8f05a33958ad04
osparking <jbpark03@gmail.com>
2023-08-11 21:44:19 +0900

    remove complex code from config class

--- src/main/java/space/bum/config/SecurityConfig.java
+++ src/main/java/space/bum/config/SecurityConfig.java
@@ -2,19 +2,11 @@ package space.bum.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import space.bum.services.JpaUserDetailsService;
-
 @Configuration
 public class SecurityConfig {
-	
-	@Bean
-	UserDetailsService userDetailsService() {
-		return new JpaUserDetailsService();
-	}
 	
 	@SuppressWarnings("deprecation")
 	@Bean
