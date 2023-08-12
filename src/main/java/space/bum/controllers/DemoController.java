28855be2af407e88e6b28760479d920825e8db95
osparking <jbpark03@gmail.com>
2023-08-12 21:47:30 +0900

    check current authentication attributes

--- src/main/java/space/bum/controllers/DemoController.java
+++ src/main/java/space/bum/controllers/DemoController.java
@@ -1,5 +1,6 @@
 package space.bum.controllers;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 	@GetMapping("/demo")
 	String demo() {
+		var auth = SecurityContextHolder.getContext().getAuthentication();
 		return "demo";
 	}
 
