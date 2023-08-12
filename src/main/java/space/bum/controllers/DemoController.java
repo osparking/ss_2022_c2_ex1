9ae0b2c05dac7a45e37523e2ca8663613f46d85c
osparking <jbpark03@gmail.com>
2023-08-12 22:04:37 +0900

    experimental code line removed

--- src/main/java/space/bum/controllers/DemoController.java
+++ src/main/java/space/bum/controllers/DemoController.java
@@ -1,6 +1,5 @@
 package space.bum.controllers;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 	@GetMapping("/demo")
 	String demo() {
-		var auth = SecurityContextHolder.getContext().getAuthentication();
 		return "demo";
 	}
 
